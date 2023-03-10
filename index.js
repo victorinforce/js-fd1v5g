9e7f28c4c31868ae235959c45fbd317204dc3a5f
victorinforce <110573260+victorinforce@users.noreply.github.com>
2023-03-10 11:54:28 -0300

    2 files modified

--- index.js
+++ index.js
@@ -3,12 +3,31 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-// appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 $(document).ready(function () {
   $('#example').DataTable({
     processing: true,
     serverSide: true,
-    ajax: './public/data.json',
+    ajax: 'https://raw.githubusercontent.com/victorinforce/js-fd1v5g/main/public/data.json',
+    /*columns: [
+      { data: 'first_name' },
+      { data: 'last_name' },
+      { data: 'position' },
+      { data: 'office' },
+      { data: 'start_date' },
+      { data: 'salary' },
+    ],*/
+    columnDefs: [
+      {
+        orderable: false,
+        className: 'select-checkbox',
+        targets: 0,
+      },
+    ],
+    select: {
+      style: 'os',
+      selector: 'td:first-child',
+    },
+    order: [[1, 'asc']],
   });
 });
