b13a8071f38481538540170bdc2458aa9aa80994
victorinforce <110573260+victorinforce@users.noreply.github.com>
2023-03-10 11:57:31 -0300

    2 files modified

--- index.js
+++ index.js
@@ -9,25 +9,25 @@ $(document).ready(function () {
     processing: true,
     serverSide: true,
     ajax: 'https://raw.githubusercontent.com/victorinforce/js-fd1v5g/main/public/data.json',
-    /*columns: [
+    columns: [
       { data: 'first_name' },
       { data: 'last_name' },
       { data: 'position' },
       { data: 'office' },
       { data: 'start_date' },
       { data: 'salary' },
-    ],*/
-    columnDefs: [
-      {
-        orderable: false,
-        className: 'select-checkbox',
-        targets: 0,
-      },
     ],
-    select: {
-      style: 'os',
-      selector: 'td:first-child',
-    },
+    // columnDefs: [
+    //   {
+    //     orderable: false,
+    //     className: 'select-checkbox',
+    //     targets: 0,
+    //   },
+    // ],
+    // select: {
+    //   style: 'os',
+    //   selector: 'td:first-child',
+    // },
     order: [[1, 'asc']],
   });
 });
